aee93a24b257b7c306150b668fedd8fa11bf2b44
cbat <cameronchen01@gmail.com>
2020-12-28 17:49:26 +0800

    add solution

---
+++ javaScript/7kyu/Return a string's even characters.js
@@ -0,0 +1,14 @@
+// 题解一：
+function evenChars(string) {
+    if(string.length < 2 || string.length > 100) return 'invalid string';
+    var arr = [];
+    for(let i=0;i<[...string].length;i++){
+        if(i%2 == 1) arr.push([...string][i]);
+    }
+    return arr;
+}
+
+// 题解二：
+function evenChars(string) {
+    return (string.length < 2 || string.length > 100) ? 'invalid string' : [...string].filter((value,index) => index%2)
+}
