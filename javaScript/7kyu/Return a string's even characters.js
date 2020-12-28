14d23b8545ef535df2e040734bae28b628a9d81b
cbat <cameronchen01@gmail.com>
2020-12-28 18:16:28 +0800

    add solution

--- javaScript/7kyu/Return a string's even characters.js
+++ javaScript/7kyu/Return a string's even characters.js
@@ -2,7 +2,7 @@
 function evenChars(string) {
     if(string.length < 2 || string.length > 100) return 'invalid string';
     var arr = [];
-    for(let i=0;i<[...string].length;i++){
+    for(let i=0;i<[...string].length;i++){  // [...string] 将字符串转成数组
         if(i%2 == 1) arr.push([...string][i]);
     }
     return arr;
