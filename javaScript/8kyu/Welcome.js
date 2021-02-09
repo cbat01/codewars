ac6a945b55ebc90c418774132039fa8d70e002b0
cbat01 <cameronchen01@gmail.com>
2021-02-09 22:52:31 +0800

    add solutions

---
+++ javaScript/8kyu/Welcome.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+function greet(language) {
+  var obj = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  return obj[language] || obj['english'];
+}
