c6687367f693c8c6a3e04c677e6e00df1802b638
cbat01 <cameronchen01@gmail.com>
2021-03-04 15:20:57 +0800

    add solutions

--- javaScript/7kyu/Clean up after your dog.js
+++ javaScript/7kyu/Clean up after your dog.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/57faa6ff9610ce181b000028
 
 function crap(x, bags, cap){
-
+    var yard = x.reduce( (a, b) => a.concat(b) );
+    return yard.includes('D') ? 'Dog!!' : bags * cap - yard.filter( val => val === '@' ).length > 0 ? 'Clean' : 'Cr@p';
 }
