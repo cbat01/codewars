5c4792f4cd8256d4bb98dbdfd2feaa14d89bf66d
cbat01 <cameronchen01@gmail.com>
2021-03-16 18:03:57 +0800

    add solutions

---
+++ javaScript/8kyu/Function 1 - hello world.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+// Write a function "greet" that returns "hello world!"
+
+function greet() {
+  return 'hello world!';
+}
+
+let greet = () => {
+  return 'hello world!';
+}
+
+let greet = () => 'hello world!';
---
+++ javaScript/8kyu/Keep Hydrated!.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+function litres(time) {
+    return Math.floor(time/2);
+}
